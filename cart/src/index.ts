87b523a02bfdb0066181459e543176413af87f50
Yahia El Tai <yahiaeltai022@gmail.com>
2022-11-04 15:25:46 +0400

    chore: deploy cart to gcp with github action v4

--- cart/src/index.ts
+++ cart/src/index.ts
@@ -3,7 +3,7 @@ import { app } from "./app";
 
 const start = async () => {
   console.log("starting....");
-  console.log("deploying with github action v2");
+  console.log("deploying with github action v3");
 
   if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI must be defined");
