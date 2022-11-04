e2d764c5cf8a530b85204f0e3a6fc32b7c4d8169
Yahia El Tai <yahiaeltai022@gmail.com>
2022-11-04 15:46:05 +0400

    chore: test again

--- cart/src/index.ts
+++ cart/src/index.ts
@@ -3,7 +3,7 @@ import { app } from "./app";
 
 const start = async () => {
   console.log("starting....");
-  console.log("deploying with github action v4 - This should work");
+  console.log("deploying with github action v4 - Now it should work");
 
   if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI must be defined");
