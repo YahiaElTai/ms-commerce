355870d7ec9e6ef830310be506b3b25fb8a6eaee
Yahia El Tai <yahiaeltai022@gmail.com>
2022-11-20 02:57:39 +0400

    chore: add new cli

--- files-changed-cli/index.js
+++ files-changed-cli/index.js
@@ -3,27 +3,42 @@
 import { execaCommand } from "execa";
 import mri from "mri";
 
-// cli will receive folder and revision branch as arguments
-// cli returns true if changed
+const FOLDERS = {
+  auth: "auth",
+  cart: "cart",
+  infra: "infra/k8s",
+};
 
-const getFilesChanged = async () => {
-  const cliFlags = mri(process.argv.slice(2));
-  console.log(cliFlags);
+const git = {
+  base: (headRevision) => `git merge-base main ${headRevision}`,
+  changedFiles: (mergeRevision, headRevision) =>
+    `git diff --name-only ${mergeRevision} ${headRevision}`,
+};
 
-  const filesChangedOutput = await execaCommand(
-    "git diff --name-only main testing"
+(async () => {
+  const cliFlags = mri(process.argv.slice(2), {
+    default: {
+      "head-revision": process.env.CIRCLE_SHA1,
+    },
+  });
+
+  const baseCmdResult = await execaCommand(git.base(cliFlags["head-revision"]));
+
+  const mergeRevision = baseCmdResult.stdout;
+  const changedFilesCmdResult = await execaCommand(
+    git.changedFiles(mergeRevision, cliFlags["head-revision"])
   );
 
-  const filesChanged = filesChangedOutput.stdout
+  const filesChanged = changedFilesCmdResult.stdout
     .split("\n")
     .map((filePath) => filePath.trim())
     .filter((filePath) => filePath.length > 0);
 
-  const authChanged = filesChanged.some((file) => file.startsWith("auth/"));
-  const cartChanged = filesChanged.some((file) => file.startsWith("cart/"));
-  const infraChanged = filesChanged.some((file) =>
-    file.startsWith("infra/k8s/")
-  );
-};
+  if (FOLDERS[cliFlags.folder]) {
+    const didFolderChange = filesChanged.some((file) =>
+      file.startsWith(`${cliFlags.folder}/`)
+    );
 
-getFilesChanged();
+    process.stdout.write("true");
+  }
+})();
