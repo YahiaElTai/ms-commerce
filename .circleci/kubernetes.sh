f07d0dca1e88b59c56a03f037a69306feb31e5a1
Yahia El Tai <yahiaeltai022@gmail.com>
2022-11-21 01:41:04 +0400

    update

--- .circleci/kubernetes.sh
+++ .circleci/kubernetes.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-gcloud container clusters get-credentials $CLUSTER_NAME --zone $GOOGLE_COMPUTE_ZONE --project $GOOGLE_PROJECT_ID
+gcloud container clusters get-credentials "$CLUSTER_NAME" --zone "$GOOGLE_COMPUTE_ZONE" --project "$GOOGLE_PROJECT_ID"
 
-if [ "$OPERATION" == "rollout" ]; then 
-        kubectl rollout restart deployment $SERVICE_NAME-depl
-        elif [ "$OPERATION" == "apply" ]; then
-            kubectl apply -f ../infra/k8s
+if [ "$OPERATION" == "rollout" ]; then
+    kubectl rollout restart deployment "$SERVICE_NAME"-depl
+elif [ "$OPERATION" == "apply" ]; then
+    kubectl apply -f ../infra/k8s
 fi
