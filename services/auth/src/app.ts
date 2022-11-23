55d3fb3e14154b8a782571487f5d043c07b24acb
Yahia El Tai <yahiaeltai022@gmail.com>
2022-11-23 15:34:24 +0400

    chore: added new config

--- services/auth/src/app.ts
+++ services/auth/src/app.ts
@@ -9,6 +9,7 @@ import { signupRouter } from './routes/signup';
 import { errorHandler, NotFoundError } from '@ms-commerce/common';
 
 const app = express();
+
 app.set('trust proxy', true);
 app.use(json());
 app.use(cookieParser());
