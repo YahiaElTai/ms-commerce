e384890779e187cc9d318dc00b583eb4180ac8b5
Yahia El Tai <yahiaeltai022@gmail.com>
2022-11-29 14:27:26 +0400

    update

--- services/auth/src/app.ts
+++ services/auth/src/app.ts
@@ -9,7 +9,6 @@ import { signupRouter } from './routes/signup';
 import { errorHandler, NotFoundError } from '@ms-commerce/common';
 
 const app = express();
-console.log('changed');
 
 app.set('trust proxy', true);
 app.use(json());
