3ab2c81abd4dd9e7fff12920f8ad07e508279f7b
Yahia El Tai <yahiaeltai022@gmail.com>
2022-12-10 21:45:34 +0100

    added config

--- .circleci/kubernetes.sh
+++ .circleci/kubernetes.sh
@@ -2,12 +2,21 @@
 
 set -e
 
+: "${GOOGLE_PROJECT_ID?Required env variable GOOGLE_PROJECT_ID}"
+: "${GOOGLE_COMPUTE_ZONE?Required env variable GOOGLE_COMPUTE_ZONE}"
+: "${CLUSTER_NAME?Required env variable CLUSTER_NAME}"
+: "${OPERATION?Required env variable OPERATION}"
+
 gcloud container clusters get-credentials "$CLUSTER_NAME" --zone "$GOOGLE_COMPUTE_ZONE" --project "$GOOGLE_PROJECT_ID"
 
-if [ "$OPERATION" = "rollout" ]; then
+case $OPERATION in
+rollout)
     kubectl rollout restart deployment "${SERVICE_NAME}-depl"
-elif [ "$OPERATION" = "apply" ]; then
+    ;;
+apply)
     kubectl apply -f ./infra/k8s
-else
-    echo "OPERATION not defined. OPERATION can be one of 2 values [ rollout, apply ]"
-fi
+    ;;
+*)
+    echo "OPERATION must be one of 2 values: rollout or apply"
+    ;;
+esac
