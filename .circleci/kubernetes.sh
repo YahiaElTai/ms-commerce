a186d2db0a21aae6f726f2bbd521a502b600e854
Yahia El Tai <yahiaeltai022@gmail.com>
2022-12-11 23:59:05 +0100

    update

--- .circleci/kubernetes.sh
+++ .circleci/kubernetes.sh
@@ -7,10 +7,29 @@ set -e
 : "${CLUSTER_NAME?Required env variable CLUSTER_NAME}"
 : "${OPERATION?Required env variable OPERATION}"
 
+apply_prisma_migrations() {
+    if [ "$SERVICE_NAME" == "auth" ]; then
+        echo "Applying pending Prisma migrations from inside K8s pod"
+
+        # Get the name of one of the settings pods running
+        pod_name=$(kubectl get pods --field-selector=status.phase=Running --sort-by=.metadata.creationTimestamp -l app="auth" -o=name | tail -1)
+
+        echo "Using pod $pod_name"
+
+        # Trigger a migration deployment using the running pod (to ensure the DB connection works)
+        kubectl exec -it "$pod_name" -- \
+            npx prisma migrate deploy --schema=./prisma/schema.prisma
+
+    else
+        echo "${SERVICE_NAME} does not use Prisma"
+    fi
+}
+
 gcloud container clusters get-credentials "$CLUSTER_NAME" --zone "$GOOGLE_COMPUTE_ZONE" --project "$GOOGLE_PROJECT_ID"
 
 case $OPERATION in
 rollout)
+    apply_prisma_migrations
     kubectl rollout restart deployment "${SERVICE_NAME}-depl"
     ;;
 apply)
