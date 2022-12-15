ae914f52b605292e116e6ff437aaff74c18e59ca
Yahia El Tai <yahiaeltai022@gmail.com>
2022-12-15 21:42:27 +0100

    chore: added new env variable for ci

--- services/auth/src/app.ts
+++ services/auth/src/app.ts
@@ -8,6 +8,7 @@ import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler, NotFoundError } from '@ms-commerce/common';
 
+console.log('Hello');
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
