5df2fc4dd3608ef6cdff13e08c4e08cc874f091b
Yahia El Tai <yahiaeltai022@gmail.com>
2022-12-22 10:12:35 +0100

    add pre-commit instead of husky

--- services/cart/.eslintrc.js
+++ services/cart/.eslintrc.js
@@ -6,13 +6,17 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
+  // parserOptions: {
+  //   tsconfigRootDir: __dirname,
+  //   project: ['./tsconfig.json'],
+  // },
   plugins: ['@typescript-eslint'],
 };
