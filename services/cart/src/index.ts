e9241f90e77050d0214d8cf6d4dcb22ae0765de0
Yahia El Tai <yahiaeltai022@gmail.com>
2022-12-25 21:23:01 +0100

    add container probes to cart service

--- services/cart/src/index.ts
+++ services/cart/src/index.ts
@@ -1,12 +1,25 @@
+import { createLightship } from 'lightship';
+
 import { app } from './app';
 
 const start = async () => {
+  const lightship = await createLightship();
+
   if (!process.env.DATABASE_URL) {
     throw new Error('DATABASE_URL must be defined');
   }
 
-  app.listen(3001, () => {
-    console.log('Listening on port 3001!');
+  const server = app
+    .listen(3001, () => {
+      console.log('Listening on port 3001!');
+      lightship.signalReady();
+    })
+    .on('error', () => {
+      lightship.shutdown();
+    });
+
+  lightship.registerShutdownHandler(() => {
+    server.close();
   });
 };
 
