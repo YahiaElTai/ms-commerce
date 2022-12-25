2821af3244bf1adad66b116b085caf7b811d8dea
Yahia El Tai <yahiaeltai022@gmail.com>
2022-12-25 12:22:09 +0100

    ensure database port is correct on ci

--- .circleci/generate_test_env.sh
+++ .circleci/generate_test_env.sh
@@ -6,18 +6,36 @@ set -e
 
 echo -e "\033[32mGenerating .env for $SERVICE_NAME service"
 
+# Each service database runs on a specific port
+# This helps with local development while running all services together
+# This switch statements decide the port number based on the service name
+case $SERVICE_NAME in
+
+auth)
+    DATABASE_URL=postgresql://prisma:prisma@localhost:5432/prisma
+    ;;
+
+cart)
+    DATABASE_URL=postgresql://prisma:prisma@localhost:5435/prisma
+    ;;
+
+*)
+    DATABASE_URL=postgresql://prisma:prisma@localhost:5432/prisma
+    ;;
+esac
+
 if [ "$SERVICE_NAME" == "auth" ]; then
 
     cat <<EOF >/home/circleci/ms-commerce/services/"$SERVICE_NAME"/.env
     NODE_ENV=test
-    DATABASE_URL="postgresql://prisma:prisma@localhost:5432/prisma"
+    DATABASE_URL="$DATABASE_URL"
     JWT_KEY=ajdkjlkajlsd
 EOF
 
 else
     cat <<EOF >/home/circleci/ms-commerce/services/"$SERVICE_NAME"/.env
     NODE_ENV=test
-    DATABASE_URL="postgresql://prisma:prisma@localhost:5432/prisma"
+    DATABASE_URL="$DATABASE_URL"
 EOF
 
 fi
