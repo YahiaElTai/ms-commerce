38ea81c55b496c8d9f2a80b53a552923b3580186
Yahia El Tai <yahiaeltai022@gmail.com>
2022-12-25 20:13:23 +0100

    add container probs to auth service with lightship

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,6 +1,10 @@
+import { createLightship } from 'lightship';
+
 import { app } from './app';
 
 const start = async () => {
+  const lightship = await createLightship();
+
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
@@ -9,8 +13,17 @@ const start = async () => {
     throw new Error('DATABASE_URL must be defined');
   }
 
-  app.listen(3000, () => {
-    console.log('Listening on port 3000!');
+  const server = app
+    .listen(3000, () => {
+      console.log('Listening on port 3000!');
+      lightship.signalReady();
+    })
+    .on('error', () => {
+      lightship.shutdown();
+    });
+
+  lightship.registerShutdownHandler(() => {
+    server.close();
   });
 };
 
