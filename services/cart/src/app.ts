8f4a3abfafac57cb66994e02bd2bff983d1eae6d
Yahia El Tai <yahiaeltai022@gmail.com>
2022-12-26 01:30:33 +0100

    remove cookie-parser from cart service

--- services/cart/src/app.ts
+++ services/cart/src/app.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import { CartsRouter } from './routes/list';
-import cookieParser from 'cookie-parser';
 import { errorHandler, NotFoundError } from '@ms-commerce/common';
 import { SingleCartRouter } from './routes/single';
 import { CreateCartRouter } from './routes/create';
@@ -11,7 +10,6 @@ import { CreateCartRouter } from './routes/create';
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
-app.use(cookieParser());
 
 app.use(CartsRouter);
 app.use(SingleCartRouter);
