59e6f7966d368004ff5814bdd4f1fca276fd90a7
Yahia El Tai <yahiaeltai022@gmail.com>
2022-12-31 13:08:56 +0100

    add docs for excluding fields

--- services/cart/src/prisma/utils.ts
+++ services/cart/src/prisma/utils.ts
@@ -1,3 +1,6 @@
+// Excluding the cartId from the result of fetching the line items on the cart just for now
+// until Prisma has this feature natively
+// https://github.com/prisma/prisma/issues/5042
 export const excludeCartIdFromLineItem = {
   id: true,
   version: true,
