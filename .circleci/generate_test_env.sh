a94e83a7674b9f21556618ac0eb683ea87b4d729
Yahia El Tai <yahiaeltai022@gmail.com>
2023-04-20 09:51:44 +0200

    chore: rewrite script in a cleaner way

--- .circleci/generate_test_env.sh
+++ .circleci/generate_test_env.sh
@@ -6,40 +6,40 @@ set -e
 
 echo -e "\033[32mGenerating .env for $SERVICE_NAME service"
 
+# Function to generate .env content
+generate_env_content() {
+    local db_url=$1
+    local jwt_key=$2
+
+    echo "NODE_ENV=test"
+    echo "DATABASE_URL=$db_url"
+
+    if [ -n "$jwt_key" ]; then
+        echo "JWT_KEY=$jwt_key"
+    fi
+}
+
 # Each service database runs on a specific port
 # This helps with local development while running all services together
 # This switch statements decide the port number based on the service name
 case $SERVICE_NAME in
-
 account)
-    DATABASE_URL=postgresql://prisma:prisma@localhost:5432/prisma
+    db_port=5432
+    jwt_key="ajdkjlkajlsd"
     ;;
-
 cart)
-    DATABASE_URL=postgresql://prisma:prisma@localhost:5435/prisma
+    db_port=5435
     ;;
-
 product)
-    DATABASE_URL=postgresql://prisma:prisma@localhost:5436/prisma
+    db_port=5436
     ;;
-
 *)
-    DATABASE_URL=postgresql://prisma:prisma@localhost:5432/prisma
+    db_port=5432
     ;;
 esac
 
-if [ "$SERVICE_NAME" == "account" ]; then
+DATABASE_URL="postgresql://prisma:prisma@localhost:$db_port/prisma"
+env_file_path="/home/circleci/ms-commerce/services/$SERVICE_NAME/.env"
 
-    cat <<EOF >/home/circleci/ms-commerce/services/"$SERVICE_NAME"/.env
-    NODE_ENV=test
-    DATABASE_URL="$DATABASE_URL"
-    JWT_KEY=ajdkjlkajlsd
-EOF
-
-else
-    cat <<EOF >/home/circleci/ms-commerce/services/"$SERVICE_NAME"/.env
-    NODE_ENV=test
-    DATABASE_URL="$DATABASE_URL"
-EOF
-
-fi
+# Generate .env content and write it to the file
+generate_env_content "$DATABASE_URL" "$jwt_key" >"$env_file_path"
