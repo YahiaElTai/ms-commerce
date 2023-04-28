2aa11f703c969cae9dfca047bdc1210338a3264a
Yahia El Tai <yahiaeltai022@gmail.com>
2023-04-28 23:57:12 +0200

    chore: export gcloud service key from script

--- .circleci/export_gcloud_service_key.sh
+++ .circleci/export_gcloud_service_key.sh
@@ -3,17 +3,15 @@
 set -e
 
 : "${ENVIRONMENT_NAME?Required env variable ENVIRONMENT_NAME}"
-
-echo -e "\033[32mSourcing environment variables..."
-# shellcheck disable=SC1090
-source ".circleci/.env.$ENVIRONMENT_NAME"
+: "${GCLOUD_SERVICE_KEY_STAGING?Required env variable GCLOUD_SERVICE_KEY_STAGING}"
+: "${GCLOUD_SERVICE_KEY_PROD?Required env variable GCLOUD_SERVICE_KEY_PROD}"
 
 echo -e "\033[32mExporting GCLOUD_SERVICE_KEY env variable for environment $ENVIRONMENT_NAME"
 
+GCLOUD_SERVICE_KEY="$GCLOUD_SERVICE_KEY_STAGING"
+
 if [ "$ENVIRONMENT_NAME" == "gcp-production-eu" ]; then
     GCLOUD_SERVICE_KEY="$GCLOUD_SERVICE_KEY_PROD"
-else
-    GCLOUD_SERVICE_KEY="$GCLOUD_SERVICE_KEY_STAGING"
 fi
 
-export GCLOUD_SERVICE_KEY
+echo "GCLOUD_SERVICE_KEY=\"$GCLOUD_SERVICE_KEY\"" >>".circleci/.env.$ENVIRONMENT_NAME"
