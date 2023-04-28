f197bdf98021e3b30b93bf569e0c090a45068234
Yahia El Tai <yahiaeltai022@gmail.com>
2023-04-29 00:56:21 +0200

    chore: add helm values and secrets per env

--- .circleci/deploy_service.sh
+++ .circleci/deploy_service.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+: "${ENVIRONMENT_NAME?Required env variable ENVIRONMENT_NAME}"
 : "${GOOGLE_PROJECT_ID?Required env variable GOOGLE_PROJECT_ID}"
 : "${GOOGLE_COMPUTE_REGION?Required env variable GOOGLE_COMPUTE_REGION}"
 : "${CLUSTER_NAME?Required env variable CLUSTER_NAME}"
@@ -35,7 +36,7 @@ if [ "$HELM_RELEASE_NAME" = "ms-ingress" ]; then
         --install \
         --wait \
         --timeout 10m0s \
-        -f "infra/k8s/$CHART_NAME/values.yaml" \
+        -f "infra/k8s/values/$ENVIRONMENT_NAME/$CHART_NAME/values.yaml" \
         "$HELM_RELEASE_NAME" "infra/k8s/$CHART_NAME"
 else
     echo -e "\033[32mDecrypting helm secrets..."
@@ -43,16 +44,16 @@ else
         --key ms-commerce-key \
         --keyring ms-commerce-key-ring \
         --location "$GOOGLE_COMPUTE_REGION" \
-        --ciphertext-file "infra/k8s/$CHART_NAME/secrets.yaml.enc" \
-        --plaintext-file "infra/k8s/$CHART_NAME/secrets.yaml"
+        --ciphertext-file "infra/k8s/values/$ENVIRONMENT_NAME/$CHART_NAME/secrets.yaml.enc" \
+        --plaintext-file "infra/k8s/values/$ENVIRONMENT_NAME/$CHART_NAME/secrets.yaml"
 
     echo -e "\033[32mUpgrading $HELM_RELEASE_NAME"
     helm upgrade \
         --install \
         --wait \
         --timeout 10m0s \
-        -f "infra/k8s/$CHART_NAME/values.yaml" \
-        -f "infra/k8s/$CHART_NAME/secrets.yaml" \
+        -f "infra/k8s/values/$ENVIRONMENT_NAME/$CHART_NAME/values.yaml" \
+        -f "infra/k8s/values/$ENVIRONMENT_NAME/$CHART_NAME/secrets.yaml" \
         --set image.tag="$GIT_REVISION" \
         "$HELM_RELEASE_NAME" "infra/k8s/$CHART_NAME"
 
