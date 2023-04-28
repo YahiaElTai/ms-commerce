7d15f83c099d1b0ca9b2c97c781ff24371ccbe1d
Yahia El Tai <yahiaeltai022@gmail.com>
2023-04-28 02:25:19 +0200

    chore: update terraform file

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -5,6 +5,16 @@ terraform {
       source  = "hashicorp/google"
       version = "4.63.0"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.20.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.9.0"
+    }
   }
 }
 
