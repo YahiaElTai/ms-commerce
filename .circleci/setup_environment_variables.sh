8af43bfce5699c73ba006ee2236c3df814b76298
Yahia El Tai <yahiaeltai022@gmail.com>
2023-05-10 01:28:59 +0200

    chore: update config

--- .circleci/setup_environment_variables.sh
+++ .circleci/setup_environment_variables.sh
@@ -6,16 +6,16 @@ set -e
 
 echo -e "\033[32mExporting enviroment variables $ENVIRONMENT_NAME"
 
-if [[ -n "${GCLOUD_SERVICE_KEY_ENV_NAME}" ]]; then
-    GCLOUD_SERVICE_KEY="${!GCLOUD_SERVICE_KEY_ENV_NAME}"
+if [[ -n "${GCP_JSON_KEY}" ]]; then
+    GCLOUD_SERVICE_KEY="${!GCP_JSON_KEY}"
 
     echo "GCLOUD_SERVICE_KEY=$GCLOUD_SERVICE_KEY" >>".circleci/.env.$ENVIRONMENT_NAME"
 
     echo -e "\033[32mExported GCLOUD_SERVICE_KEY for enviroment $ENVIRONMENT_NAME"
 fi
 
-if [[ -n "${TERRAFORM_SERVICE_KEY_ENV_NAME}" ]]; then
-    TERRAFORM_SERVICE_KEY="${!TERRAFORM_SERVICE_KEY_ENV_NAME}"
+if [[ -n "${TERRAFORM_JSON_KEY}" ]]; then
+    TERRAFORM_SERVICE_KEY="${!TERRAFORM_JSON_KEY}"
     echo "$TERRAFORM_SERVICE_KEY" >>"$HOME"/terraform-service-key.json
 
     echo "GOOGLE_APPLICATION_CREDENTIALS=\"$HOME/terraform-service-key.json\"" >>".circleci/.env.$ENVIRONMENT_NAME"
