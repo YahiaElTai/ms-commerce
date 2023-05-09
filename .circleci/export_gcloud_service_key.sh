4c64995bb055614d213a94ecfdfdac7759e5682a
Yahia El Tai <yahiaeltai022@gmail.com>
2023-05-09 20:58:22 +0200

    chore: update config file

--- .circleci/export_gcloud_service_key.sh
+++ .circleci/export_gcloud_service_key.sh
@@ -3,15 +3,19 @@
 set -e
 
 : "${ENVIRONMENT_NAME?Required env variable ENVIRONMENT_NAME}"
-: "${GCLOUD_SERVICE_KEY_STAGING?Required env variable GCLOUD_SERVICE_KEY_STAGING}"
-: "${GCLOUD_SERVICE_KEY_PROD?Required env variable GCLOUD_SERVICE_KEY_PROD}"
+# : "${GCLOUD_SERVICE_KEY_STAGING?Required env variable GCLOUD_SERVICE_KEY_STAGING}"
+# : "${GCLOUD_SERVICE_KEY_PROD?Required env variable GCLOUD_SERVICE_KEY_PROD}"
 
-echo -e "\033[32mExporting GCLOUD_SERVICE_KEY env variable for environment $ENVIRONMENT_NAME"
+# echo -e "\033[32mExporting GCLOUD_SERVICE_KEY env variable for environment $ENVIRONMENT_NAME"
 
-GCLOUD_SERVICE_KEY="$(printf %q "$GCLOUD_SERVICE_KEY_STAGING")"
+# GCLOUD_SERVICE_KEY="$(printf %q "$GCLOUD_SERVICE_KEY_STAGING")"
 
-if [ "$ENVIRONMENT_NAME" == "gcp-production-eu" ]; then
-    GCLOUD_SERVICE_KEY="$(printf %q "$GCLOUD_SERVICE_KEY_PROD")"
-fi
+# if [ "$ENVIRONMENT_NAME" == "gcp-production-eu" ]; then
+#     GCLOUD_SERVICE_KEY="$(printf %q "$GCLOUD_SERVICE_KEY_PROD")"
+# fi
 
-echo "GCLOUD_SERVICE_KEY=$GCLOUD_SERVICE_KEY" >>".circleci/.env.$ENVIRONMENT_NAME"
+# echo "GCLOUD_SERVICE_KEY=$GCLOUD_SERVICE_KEY" >>".circleci/.env.$ENVIRONMENT_NAME"
+
+echo "$TERRAFORM_GCP_STAGING_KEY" >"$HOME"/terraform-service-key.json
+
+echo "GOOGLE_APPLICATION_CREDENTIALS=$HOME/terraform-service-key.json" >>".circleci/.env.$ENVIRONMENT_NAME"
