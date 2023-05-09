8a1147cfdb1c45e827b6fdbe07cdb9b89ace61ca
Yahia El Tai <yahiaeltai022@gmail.com>
2023-05-10 01:39:28 +0200

    chore: update config

--- .circleci/setup_environment_variables.sh
+++ .circleci/setup_environment_variables.sh
@@ -6,16 +6,18 @@ set -e
 
 echo -e "\033[32mExporting enviroment variables $ENVIRONMENT_NAME"
 
-if [[ -n "${GCP_JSON_KEY}" ]]; then
-    GCLOUD_SERVICE_KEY="${!GCP_JSON_KEY}"
+if [[ -n "${GCLOUD_SERVICE_KEY_ENV_NAME}" ]]; then
+    GCLOUD_SERVICE_KEY_NAME="${!GCLOUD_SERVICE_KEY_ENV_NAME}"
+    GCLOUD_SERVICE_KEY="${!GCLOUD_SERVICE_KEY_NAME}"
 
     echo "GCLOUD_SERVICE_KEY=$GCLOUD_SERVICE_KEY" >>".circleci/.env.$ENVIRONMENT_NAME"
 
     echo -e "\033[32mExported GCLOUD_SERVICE_KEY for enviroment $ENVIRONMENT_NAME"
 fi
 
-if [[ -n "${TERRAFORM_JSON_KEY}" ]]; then
-    TERRAFORM_SERVICE_KEY="${!TERRAFORM_JSON_KEY}"
+if [[ -n "${TERRAFORM_SERVICE_KEY_ENV_NAME}" ]]; then
+    TERRAFORM_SERVICE_KEY_NAME="${!TERRAFORM_SERVICE_KEY_ENV_NAME}"
+    TERRAFORM_SERVICE_KEY="${!TERRAFORM_SERVICE_KEY_NAME}"
     echo "$TERRAFORM_SERVICE_KEY" >>"$HOME"/terraform-service-key.json
 
     echo "GOOGLE_APPLICATION_CREDENTIALS=\"$HOME/terraform-service-key.json\"" >>".circleci/.env.$ENVIRONMENT_NAME"
