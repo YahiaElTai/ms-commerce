91052b5f5c5a2800dbc1f0454ec98d6df9eb7f94
Yahia El Tai <yahiaeltai022@gmail.com>
2023-05-10 22:17:44 +0200

    chore: add k8s and helm providers with terraform

--- infra/terraform/module_data.tf
+++
@@ -1,6 +0,0 @@
-# fetch k8s cluster information from GKE to authenticate k8s and helm providers
-data "google_container_cluster" "ms_commerce_cluster_info" {
-  name     = var.k8s_cluster_name
-  project  = var.project_id
-  location = var.region
-}
--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -4,18 +4,25 @@ provider "google" {
   zone    = var.zone
 }
 
+# fetch k8s cluster information from GKE to authenticate k8s and helm providers
+data "google_container_cluster" "ms_commerce_cluster_info" {
+  name     = var.k8s_cluster_name
+  project  = var.project_id
+  location = var.region
+}
+
 provider "kubernetes" {
-  host                   = "https://${data.google_container_cluster.ms-commerce-cluster-info.endpoint}"
-  client_certificate     = base64decode(data.google_container_cluster.ms-commerce-cluster-info.master_auth[0].client_certificate)
-  client_key             = base64decode(data.google_container_cluster.ms-commerce-cluster-info.master_auth[0].client_key)
-  cluster_ca_certificate = base64decode(data.google_container_cluster.ms-commerce-cluster-info.master_auth[0].cluster_ca_certificate)
+  host                   = "https://${data.google_container_cluster.ms_commerce_cluster_info.endpoint}"
+  client_certificate     = base64decode(data.google_container_cluster.ms_commerce_cluster_info.master_auth[0].client_certificate)
+  client_key             = base64decode(data.google_container_cluster.ms_commerce_cluster_info.master_auth[0].client_key)
+  cluster_ca_certificate = base64decode(data.google_container_cluster.ms_commerce_cluster_info.master_auth[0].cluster_ca_certificate)
 }
 
 provider "helm" {
   kubernetes {
-    host                   = "https://${data.google_container_cluster.ms-commerce-cluster-info.endpoint}"
-    client_certificate     = base64decode(data.google_container_cluster.ms-commerce-cluster-info.master_auth[0].client_certificate)
-    client_key             = base64decode(data.google_container_cluster.ms-commerce-cluster-info.master_auth[0].client_key)
-    cluster_ca_certificate = base64decode(data.google_container_cluster.ms-commerce-cluster-info.master_auth[0].cluster_ca_certificate)
+    host                   = "https://${data.google_container_cluster.ms_commerce_cluster_info.endpoint}"
+    client_certificate     = base64decode(data.google_container_cluster.ms_commerce_cluster_info.master_auth[0].client_certificate)
+    client_key             = base64decode(data.google_container_cluster.ms_commerce_cluster_info.master_auth[0].client_key)
+    cluster_ca_certificate = base64decode(data.google_container_cluster.ms_commerce_cluster_info.master_auth[0].cluster_ca_certificate)
   }
 }
