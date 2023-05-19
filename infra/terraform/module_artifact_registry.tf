1fba203b01599fc2453aaf491f2cdf0ee4c3e6e7
Yahia El Tai <yahiaeltai022@gmail.com>
2023-05-19 21:49:16 +0200

    chore: cleanup infra

--- infra/terraform/module_artifact_registry.tf
+++ infra/terraform/module_artifact_registry.tf
@@ -1,12 +1,12 @@
-# Create Artifact Registry repository
-resource "google_artifact_registry_repository" "ms_commerce_repository" {
-  repository_id = var.repository_name
-  location      = var.region
-  format        = "DOCKER"
-}
+# # Create Artifact Registry repository
+# resource "google_artifact_registry_repository" "ms_commerce_repository" {
+#   repository_id = var.repository_name
+#   location      = var.region
+#   format        = "DOCKER"
+# }
 
-# Output for the Artifact Registry repository ID
-output "artifact_registry_repository_id" {
-  value       = google_artifact_registry_repository.ms_commerce_repository.id
-  description = "The ID of the created Artifact Registry repository."
-}
+# # Output for the Artifact Registry repository ID
+# output "artifact_registry_repository_id" {
+#   value       = google_artifact_registry_repository.ms_commerce_repository.id
+#   description = "The ID of the created Artifact Registry repository."
+# }
